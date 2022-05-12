db756ca479d3a57efb5060846b1eddf09da9ab10
Onintsoa200 <77721908+Onintsoa200@users.noreply.github.com>
2022-05-12 15:32:34 +0300

    [suite]

--- routes/routes.js
+++ routes/routes.js
@@ -4,11 +4,15 @@ const router = express.Router();
 const MatiereController = require('../controllers/matiereController');
 
 const UserController = require('../controllers/uersController');
+const vocab = require('../controllers/vocabulaire.controller.js');
 
 router.post('/addmatiere', MatiereController.addMatiere);
 
 router.post('/addUser', UserController.addUser);
 router.post('/authentification', UserController.authentification);
 router.get('/getToken', UserController.getToken);
+router.post('/addmatiere', MatiereController.addMatiere);
+router.get('/getByType', vocab.findByType);
 
 module.exports = router;
+
